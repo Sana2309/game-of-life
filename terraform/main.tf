4d3a9a6d680f0a5f3497c12da4dd0401777fd6af
Sana2309 <130492477+Sana2309@users.noreply.github.com>
2026-01-19 21:53:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,15 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+resource "aws_instance" "game_server" {
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = "t2.micro"
+  key_name               = "my-key"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "game-of-life-ec2"
+  }
+}
