52012cc00c11035b8b039ea6aea705a88a165a61
Sana2309 <130492477+Sana2309@users.noreply.github.com>
2026-01-19 21:49:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,21 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+resource "aws_security_group" "alb_sg" {
+  name = "alb-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
